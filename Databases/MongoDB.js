f46132bcd0bacf99b5edd3164cbf4a3c76a3b56b
elfilali <mohamed.didje@hotmail.com>
2018-07-09 11:51:04 +0100

    init commit

---
+++ Databases/MongoDB.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import { env } from '../lib/env'
+import { logger } from '../lib/logger'
+
+export const connectToDB = async () => {
+  try {
+    mongoose.connect(`mongodb://${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`)
+    logger.info('Connected to mongo!!!')
+  } catch (err) {
+    logger.error('Could not connect to MongoDB')
+  }
+}
+
+export const closeDB = () => {
+  mongoose.connection.close()
+  logger.info('MongoDB connection closed!!!')
+}
